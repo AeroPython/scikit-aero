fdcdfa604bb4f69dc7b4d01f3e431fda48cdc6dc
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2013-02-24 11:38:09 +0100

    Oblique shocks manipulation

--- skaero/gasdynamics/shocks.py
+++ skaero/gasdynamics/shocks.py
@@ -3,6 +3,12 @@
 """
 Shock waves.
 
+The following notation is used:
+
+* `X_1` property X in front of the shock.
+* `X_2` property X behind the shock.
+* `X2_X1` ratio between `X_2` and `X_1`.
+
 Classes
 -------
 NormalShock(M_1, gamma)
@@ -18,9 +24,147 @@ Examples
 from __future__ import division
 
 import numpy as np
+import scipy as sp
+import scipy.optimize
 
 
-class NormalShock(object):
+def from_deflection_angle(M_1, theta, weak=True, gamma=1.4):
+    """Returns oblique shock given incident Mach number and deflection angle.
+
+    By default weak solution is selected, unless weak=False is provided.
+
+    """
+    def eq(beta, M_1, theta, gamma):
+        os = ObliqueShock(M_1, beta, gamma)
+        return os.theta - theta
+
+    theta_max, beta_theta_max = max_deflection(M_1)
+    if theta > theta_max:
+        raise ValueError("No solution for this deflection angle")
+    else:
+        if weak:
+            mu = np.arcsin(1 / M_1)
+            beta = sp.optimize.bisect(
+                eq, mu, beta_theta_max, args=(M_1, theta, gamma))
+        else:
+            beta = sp.optimize.bisect(
+                eq, beta_theta_max, np.pi / 2, args=(M_1, theta, gamma))
+
+    return ObliqueShock(M_1, beta, gamma)
+
+
+def max_deflection(M_1, gamma=1.4):
+    """Returns maximum deflection angle and corresponding wave angle for given
+    Mach number.
+
+    """
+    def eq(beta, M_1, gamma):
+        os = ObliqueShock(M_1, beta, gamma)
+        return -os.theta
+
+    beta_0 = np.radians(50)
+    beta_theta_max, = sp.optimize.fmin(eq, beta_0, args=(M_1, gamma))
+    os = ObliqueShock(M_1, beta_theta_max, gamma)
+    return os.theta, os.beta
+
+
+class ObliqueShock(object):
+    """Class representing an oblique shock.
+
+    Parameters
+    ----------
+    M_1 : float
+        Incident Mach number.
+    beta : float
+        Shock wave angle, with respect to the incident velocity, in radians.
+    gamma : float, optional
+        Specific heat ratio, default 7 / 5.
+
+    Raises
+    ------
+    ValueError
+        If the incident Mach number is less than one.
+
+    """
+    def __init__(self, M_1, beta, gamma=1.4):
+        with np.errstate(invalid='raise'):
+            try:
+                mu = np.arcsin(1.0 / M_1)
+                if beta < mu:
+                    raise ValueError(
+                        "Shock wave angle must be higher "
+                        "than Mach angle {:.2f}°".format(np.degrees(mu)))
+            except FloatingPointError:
+                raise ValueError("Incident Mach number must be supersonic")
+
+        self.M_1 = M_1
+        self.M_1n = M_1 * np.sin(beta)
+        self.beta = beta
+        self.gamma = gamma
+
+    @property
+    def theta(self):
+        """Deflection angle of the shock.
+
+        """
+        theta = np.arctan(
+            2 / np.tan(self.beta) *
+            (np.sin(self.beta) ** 2 - 1 / (self.M_1 * self.M_1)) /
+            (self.gamma + np.cos(2 * self.beta) + 2 / (self.M_1 * self.M_1))
+        )
+        return theta
+
+    @property
+    def M_2n(self):
+        """Normal Mach number behind the shock.
+
+        """
+        M_2n = np.sqrt(
+            (1 / (self.M_1n * self.M_1n) + (self.gamma - 1) / 2) /
+            (self.gamma - (self.gamma - 1) / 2 / (self.M_1n * self.M_1n))
+        )
+        return M_2n
+
+    @property
+    def M_2(self):
+        """Mach number behind the shock.
+
+        """
+        M_2 = self.M_2n / np.sin(self.beta - self.theta)
+        return M_2
+
+    @property
+    def rho2_rho1(self):
+        """Density ratio accross the shock.
+
+        """
+        rho2_rho1 = (
+            (self.gamma + 1) /
+            (2 / (self.M_1n * self.M_1n) + self.gamma - 1)
+        )
+        return rho2_rho1
+
+    @property
+    def p2_p1(self):
+        """Pressure ratio across the shock.
+
+        """
+        p2_p1 = (
+            1 + 2 * self.gamma *
+            (self.M_1n * self.M_1n - 1) / (self.gamma + 1)
+        )
+        return p2_p1
+
+    @property
+    def T2_T1(self):
+        """Temperature ratio accross the shock.
+
+        """
+        T2_T1 = self.p2_p1 / self.rho2_rho1
+        return T2_T1
+
+
+class NormalShock(ObliqueShock):
     """Class representing a normal shock.
 
     Parameters
@@ -37,95 +181,11 @@ class NormalShock(object):
 
     """
     def __init__(self, M_1, gamma=1.4):
-        if M_1 < 1.0:
-            raise ValueError("Incident Mach number must be supersonic.")
-
-        self.gamma = gamma
-        self.M_1 = M_1
-
-    @property
-    def M_2(self):
-        """Mach number behind the shock.
-
-        """
-        M_2 = np.sqrt(
-            (1 + (self.gamma - 1) * self.M_1 * self.M_1 / 2) /
-            (self.gamma * self.M_1 * self.M_1 - (self.gamma - 1) / 2)
-        )
-        return M_2
-
-    @property
-    def p2_p1(self):
-        """Pressure ratio across the shock.
-
-        Resultant pressure over incident pressure.
-
-        """
-        p2_p1 = (
-            1 + 2 * self.gamma *
-            (self.M_1 * self.M_1 - 1) / (self.gamma + 1)
-        )
-        return p2_p1
-
-
-class ObliqueShock(object):
-    """Class representing an oblique shock.
-
-    Parameters
-    ----------
-    M_1 : float
-        Incident Mach number.
-    beta : float
-        Shock wave angle, with respect to the incident velocity, in radians.
-    strong : boolean, optional
-        Parameter to specify strong shock solution, default to False
-        (weak shock).
-    gamma : float, optional
-        Specific heat ratio, default 7 / 5.
-
-    Raises
-    ------
-    ValueError
-        If the incident Mach number is less than one.
-
-    TODO
-    ----
-    > Although the M_1 - theta approach seems to be the "right" way to
-    characterize an oblique shock (because we usually know the corner
-    angle and we want to compute the shock wave angle) this can be
-    computationally cumbersone, because
-
-    * There is the double-valued thing.
-    * There is the max angle thing (which involves computing it first).
-
-    _Nevertheless_, why not characterizing an oblique shock by its angle
-    and create a function that _returns_ an ObliqueShock given a corner
-    angle? That would be so awesome.
-
-    I need some tests and example cases before that.
-
-    > On the other hand, a normal shock is a special case of an oblique
-    shock (beta = pi / 2), so maybe I could do a nice subclassing here.
-
-    """
-    def __init__(self, M_1, beta, gamma=1.4):
-        M_1n = M_1 * np.sin(beta)
-        if M_1n < 1.0:
-            raise ValueError("Normal incident Mach number must be supersonic.")
-        self.M_1 = M_1
-        self.M_1n = M_1n
-        self.beta = beta
-        self.gamma = gamma
+        super(NormalShock, self).__init__(M_1, np.pi / 2, gamma)
 
     @property
     def theta(self):
         """Deflection angle of the shock.
 
         """
-        tan_theta = (
-            2 / np.tan(self.beta) *
-            (self.M_1 * self.M_1 * np.sin(self.beta) ** 2 - 1) /
-            (self.M_1 * self.M_1 * (self.gamma + np.cos(2 * self.beta)) + 2)
-        )
-        theta = np.arctan(tan_theta)
-        return theta, tan_theta
+        return 0.0
--- tests/test_shocks.py
+++ tests/test_shocks.py
@@ -3,6 +3,13 @@
 """
 Tests of the shocks package.
 
+References
+----------
+1. NACA-TR-1135 http://hdl.handle.net/2060/19930091059
+2. NASA-TN-D-2221 http://hdl.handle.net/2060/19640007246
+3. Normal shock tables on Wikipedia
+   http://en.wikipedia.org/wiki/Normal_shock_tables
+
 """
 
 from __future__ import division
@@ -35,8 +42,6 @@ def test_normal_shock_default_adiabatic_index():
 def test_normal_shock_M_2():
     """Tests the computation of post-shock Mach number.
 
-    Data from http://en.wikipedia.org/wiki/Normal_shock_tables.
-
     """
     M_1_list = [1.5, 1.8, 2.1, 3.0]
     M_2_list = [
@@ -59,10 +64,8 @@ def test_normal_shock_fails_asubsonic_M_1():
         shocks.NormalShock(0.8)
 
 
-def test_normal_shock_pressure_ratio():
-    """Tests normal shock pressure ratio.
-
-    Data from http://en.wikipedia.org/wiki/Normal_shock_tables.
+def test_normal_shock_ratios():
+    """Tests normal shock ratios of thermodynamics properties.
 
     """
     M_1_list = [1.5, 1.8, 2.1, 3.0]
@@ -72,9 +75,89 @@ def test_normal_shock_pressure_ratio():
         4.9783,
         10.3333
     ]
+    rho_ratio_list = [
+        1.8621,
+        2.3592,
+        2.8119,
+        3.8571
+    ]
+    T_ratio_list = [
+        1.3202,
+        1.5316,
+        1.7705,
+        2.6790
+    ]
     ns_list = [shocks.NormalShock(M_1, 1.4) for M_1 in M_1_list]
 
     for i in range(len(ns_list)):
         np.testing.assert_almost_equal(
-            ns_list[i].p2_p1, p_ratio_list[i], decimal=4
-        )
+            ns_list[i].p2_p1, p_ratio_list[i], decimal=4)
+        np.testing.assert_almost_equal(
+            ns_list[i].rho2_rho1, rho_ratio_list[i], decimal=4)
+        np.testing.assert_almost_equal(
+            ns_list[i].T2_T1, T_ratio_list[i], decimal=4)
+
+
+def test_normal_shock_infinite_limit():
+    gamma = 1.4
+    ns = shocks.NormalShock(np.inf, gamma)
+    np.testing.assert_almost_equal(
+        ns.M_2, np.sqrt((gamma - 1) / 2 / gamma), decimal=3)
+    np.testing.assert_almost_equal(
+        ns.rho2_rho1, (gamma + 1) / (gamma - 1), decimal=3)
+
+
+def test_normal_shock_zero_deflection():
+    ns = shocks.NormalShock(2.0)
+    assert ns.theta == 0.0
+
+
+def test_error_max_deflection():
+    with pytest.raises(ValueError):
+        shocks.from_deflection_angle(5, np.radians(50))
+
+
+def test_error_mach_angle():
+    with pytest.raises(ValueError):
+        shocks.ObliqueShock(5, np.radians(10))
+
+
+def test_max_deflection():
+    M_1_list = [1.4, 1.9, 2.2, 3.0, np.inf]
+    theta_max_degrees_list = [
+        9.427,
+        21.17,
+        26.1,
+        34.07,
+        45.58
+    ]
+    beta_theta_max_degrees_list = [
+        67.72,
+        64.78,
+        64.62,
+        65.24,
+        67.79
+    ]
+    angles_pairs_list = [shocks.max_deflection(M_1, 1.4) for M_1 in M_1_list]
+
+    for i in range(len(angles_pairs_list)):
+        np.testing.assert_almost_equal(
+            angles_pairs_list[i][0], np.radians(theta_max_degrees_list[i]),
+            decimal=3)
+        np.testing.assert_almost_equal(
+            angles_pairs_list[i][1],
+            np.radians(beta_theta_max_degrees_list[i]), decimal=3)
+
+
+def test_oblique_shock_from_deflection_angle():
+    # Anderson, example 4.1
+    # Notice that only graphical accuracy is achieved in the original example
+    M_1 = 3.0
+    theta = np.radians(20.0)
+    os = shocks.from_deflection_angle(M_1, theta, weak=True)
+
+    np.testing.assert_almost_equal(os.M_1n, 1.839, decimal=2)
+    np.testing.assert_almost_equal(os.M_2n, 0.6078, decimal=2)
+    np.testing.assert_almost_equal(os.M_2, 1.988, decimal=1)
+    np.testing.assert_almost_equal(os.p2_p1, 3.783, decimal=1)
+    np.testing.assert_almost_equal(os.T2_T1, 1.562, decimal=2)
