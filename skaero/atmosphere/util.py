3f3c11fc1bd3794d64265ec5ed2a2a990a4b9bea
Gillu13 <Gillu13@users.noreply.github.com>
2014-05-10 12:40:30 +0200

    change the util file by modifying geometric_to_geopotential function (Earth radius is now an optional parameter) and adding geopotential_to_geometric

--- skaero/atmosphere/util.py
+++ skaero/atmosphere/util.py
@@ -7,17 +7,18 @@ Utilities for atmospheric calculations.
 
 from __future__ import division, absolute_import
 
-R_Earth = 6369.0e3  # m
 
-
-def geometric_to_geopotential(z):
+def geometric_to_geopotential(z, R_Earth = 6369.0e3):
     """Returns geopotential altitude from geometric altitude.
 
     Parameters
     ----------
     z : array_like
         Geometric altitude in meters.
-
+        
+    R_Earth : array_like, optional
+        Effective Earth radius in meters. Default is 6369000.
+    
     Returns
     -------
     h : array_like
@@ -26,3 +27,29 @@ def geometric_to_geopotential(z):
     """
     h = z * R_Earth / (z + R_Earth)
     return h
+    
+def geopotential_to_geometric(h, R_Earth = 6369.0e3):
+    """Returns geometric altitude from geopotential altitude.
+
+    Parameters
+    ----------
+    h : array_like
+        Geopotential altitude in meters.
+        
+    R_Earth : array_like, optional
+        Effective Earth radius in meters. Default is 6369000.
+    
+    Returns
+    -------
+    z : array_like
+        Geometric altitude in meters.
+    
+    Notes
+    -----
+    Based on eq. 19 of the U.S. 1976 Standard Atmosphere.
+
+    .. _`U.S. 1976 Standard Atmosphere`
+        
+    """
+    z = h * R_Earth / (R_Earth - h)
+    return z
